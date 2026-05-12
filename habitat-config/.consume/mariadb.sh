cc67f11317003788f887ccbd4a8f20a69c3a4669
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-05-12 16:14:56 +0200

    makes initdb config optional

--- habitat-config/.consume/mariadb.sh
+++ habitat-config/.consume/mariadb.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 SOURCE_PATH_CONFIG="$1/config"; [ -d "$SOURCE_PATH_CONFIG" ] || { echo "'$SOURCE_PATH_CONFIG' not found."; exit 1; }
-SOURCE_PATH_INITDB="$1/initdb"; [ -d "$SOURCE_PATH_INITDB" ] || { echo "'$SOURCE_PATH_INITDB' not found."; exit 1; }
+SOURCE_PATH_INITDB="$1/initdb"; [ -d "$SOURCE_PATH_INITDB" ] || { echo "'$SOURCE_PATH_INITDB' not found."; }
 TARGET_PATH_CONFIG="/habitat-config/target/mariadb/config"
 TARGET_PATH_INITDB="/habitat-config/target/mariadb/initdb"
 
@@ -12,9 +12,11 @@ rm -rf "${TARGET_PATH_CONFIG:?}/"{*,.*} &>/dev/null
 echo "Copying new configuration to '$TARGET_PATH_CONFIG'"
 cp -rp "$SOURCE_PATH_CONFIG/." "$TARGET_PATH_CONFIG/"
 
-echo "Cleaning previous configuration at '$TARGET_PATH_INITDB'"
-rm -rf "${TARGET_PATH_INITDB:?}/"{*,.*} &>/dev/null
-echo "Copying new configuration to '$TARGET_PATH_INITDB'"
-cp -rp "$SOURCE_PATH_INITDB/." "$TARGET_PATH_INITDB/"
+[ -d "$SOURCE_PATH_INITDB" ] && {
+    echo "Cleaning previous configuration at '$TARGET_PATH_INITDB'"
+    rm -rf "${TARGET_PATH_INITDB:?}/"{*,.*} &>/dev/null
+    echo "Copying new configuration to '$TARGET_PATH_INITDB'"
+    cp -rp "$SOURCE_PATH_INITDB/." "$TARGET_PATH_INITDB/"
+}
 
 exit 0
