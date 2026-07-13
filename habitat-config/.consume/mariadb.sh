8a88120d50b539a93c93ac4d48ffefdd22f4cf12
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-07-13 10:36:48 +0200

    reverts rename and adapts consumer to correctly copy over provisioning files

--- habitat-config/.consume/mariadb.sh
+++ habitat-config/.consume/mariadb.sh
@@ -3,20 +3,20 @@
 set -euo pipefail
 
 SOURCE_PATH_CONFIG="$1/config"; [ -d "$SOURCE_PATH_CONFIG" ] || { echo "'$SOURCE_PATH_CONFIG' not found."; exit 1; }
-SOURCE_PATH_INITDB="$1/initdb"; [ -d "$SOURCE_PATH_INITDB" ] || { echo "'$SOURCE_PATH_INITDB' not found."; }
+SOURCE_PATH_PROVISIONING="$1/provisioning"; [ -d "$SOURCE_PATH_PROVISIONING" ] || { echo "'$SOURCE_PATH_PROVISIONING' not found."; }
 TARGET_PATH_CONFIG="/habitat-config/target/mariadb/config"
-TARGET_PATH_INITDB="/habitat-config/target/mariadb/initdb"
+TARGET_PATH_PROVISIONING="/habitat-config/target/mariadb/provisioning"
 
 echo "Cleaning previous configuration at '$TARGET_PATH_CONFIG'"
 rm -rf "${TARGET_PATH_CONFIG:?}/"{*,.*} &>/dev/null
 echo "Copying new configuration to '$TARGET_PATH_CONFIG'"
 cp -rp "$SOURCE_PATH_CONFIG/." "$TARGET_PATH_CONFIG/"
 
-[ -d "$SOURCE_PATH_INITDB" ] && {
-    echo "Cleaning previous configuration at '$TARGET_PATH_INITDB'"
-    rm -rf "${TARGET_PATH_INITDB:?}/"{*,.*} &>/dev/null
-    echo "Copying new configuration to '$TARGET_PATH_INITDB'"
-    cp -rp "$SOURCE_PATH_INITDB/." "$TARGET_PATH_INITDB/"
+[ -d "$SOURCE_PATH_PROVISIONING" ] && {
+    echo "Cleaning previous configuration at '$TARGET_PATH_PROVISIONING'"
+    rm -rf "${TARGET_PATH_PROVISIONING:?}/"{*,.*} &>/dev/null
+    echo "Copying new configuration to '$TARGET_PATH_PROVISIONING'"
+    cp -rp "$SOURCE_PATH_PROVISIONING/." "$TARGET_PATH_PROVISIONING/"
 }
 
 exit 0
